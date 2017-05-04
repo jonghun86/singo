d49d7c295a56b840ae9da73b7c671be12efeaed1
jonghun.yoo <mmyjh86@gmail.com>
2017-05-04 15:19:33 +0900

    missing a file

---
+++ app/src/main/java/com/ky/singo/Web_Cookie.java
@@ -0,0 +1,24 @@
+package com.ky.singo;
+
+/**
+ * Created by mmyjh on 2017-04-25.
+ */
+public class Web_Cookie {
+  private static Web_Cookie  cookieInstance = new Web_Cookie ();
+  private String key;
+
+  public static Web_Cookie  getInstance() {
+    return cookieInstance;
+  }
+
+  // This singletone class ban the use of default constructor
+  private Web_Cookie () { key = null; }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+
+  public String getKey() {
+    return key;
+  }
+}
